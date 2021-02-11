b135f33bce8c1b7e565bb284578417be6f055c0b
Andrian Nebeso <andrian.nebeso@pm.me>
2021-02-11 19:42:52 +0200

    Initial commit

---
+++ News List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News List
+//
+//  Created by Andrian Nebeso on 2/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
